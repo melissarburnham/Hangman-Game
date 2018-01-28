8c54af721d86b88827049da780782cd15a5f0145
Melissa Burnham <melissaburnham@Melissas-MacBook-Air.local>
2018-01-28 16:34:26 -0500

    began html and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+//Create an array of words
+
+var wordChoices = ["South Carolina", "North Carolina", "Georgia","Virginia", "New Jersey", "Connecticut", "Massachusetts", "Rhode Island", "Maryland", "Delaware", "New Hampshire", "New York", "Pennsylvania"];
+
+// var userWins = 0;
+
+
+// Randomly chooses a choice from the options array.
+var randomWord = Math.floor(Math.random() * wordChoices.length);
+var rightLetter = [];
+var wrongLetter = [];
+var underScore = [];
+var chosenWord = wordChoices[randomWord];
+
+//testing
+console.log(chosenWord);
+
+
+//Create underscore based on word
+var generateUnderscore = () => {
+	for (var i = 0; i < chosenWord.length; i++) {
+		underScore.push('_'); 
+	}
+	return underScore;
+}
+
+console.log(generateUnderscore());
+
+
+//User presses key to guess
+    document.addEventListener('keypress', (event) => {
+    	var keyCode = event.keyCode;
+    	var keyLetter = String.fromCharCode(keyCode);
+
+//if users guess is right
+    	if(chosenWord.indexOf(keyLetter) > -1) {
+    		//add to right letter array
+    		rightLetter.push(keyLetter);
+    		
+
+    		//replace underscore with right letter
+    		// underScore[chosenWord.indexOf(keyLetter)] = keyLetter
+
+    		// if(underScore.join('') " " (chosenWord){
+    		// 	//play a song and post a picture
+    		// }
+    	
+
+    	// else (wrongLetter.push(keyLetter))
+    	// 		console.log(keyLetter);
+    	
+  
+
+    //   // Determines which key was pressed.
+    //   var userGuess = event.key;
+
+	//if correct, display letter in place of correct dash
+	//if wrong, number of guesses decreases by 1
+//If user presses same key twice, nothing happens
+//When user gets word correct, new message, picture, and song
+//New round resets with new word while previous song plays	
+	
+
+
+ 
+
